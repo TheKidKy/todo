02cc18f82907ae5099b8ffd6cb0792b0c0941802
TheKidKy <imsajadky@gmail.com>
2023-10-24 16:05:09 +0330

    create todo serializer

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # EXTERNAL MODULES
+    'rest_framework',
+    # INTERNAL MODULES
+    'todo'
 ]
 
 MIDDLEWARE = [
