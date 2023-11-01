cae49a5522784ba9634bf53ddd8199ebf964d2bc
TheKidKy <imsajadky@gmail.com>
2023-11-01 09:40:38 +0330

    add perform_create to TodoViewSet

--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,7 @@ from .models import Todo
 class TodoViewSet(ModelViewSet):
     queryset = Todo.objects.all().order_by('priority')
     serializer_class = TodoSerializer
+
+    def perform_create(self, serializer):
+        # sends user id to the serializer
+        serializer.save(user=self.request.user)
